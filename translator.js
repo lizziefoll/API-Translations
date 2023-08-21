704ca32ad9ac3af31b06925c9801def862344227
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-08-21 09:12:28 -0300

    Add files via upload

---
+++ translator.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("inputText"); 
+
+function preencherFormulario(traduSaida){
+    document.getElementById("saida").innerHTML = traduSaida.translated; 
+}
+
+async function traducao(){
+    var textToTranslate = input.value; 
+    const apiUrl = `https://api.funtranslations.com/translate/minion.json?text=${textToTranslate}`; 
+
+
+        const dados = await fetch(apiUrl); 
+        console.log(dados); 
+        const tradu = await dados.json(); 
+        console.log(tradu); 
+
+        if (tradu.success.total == 1){
+            preencherFormulario(tradu.contents)
+        }else{
+            alert("error")
+        }
+
+        preencherFormulario(tradu.contents);
+
+    }
+
+document.getElementById("botao")
+    .addEventListener("click", traducao); 
